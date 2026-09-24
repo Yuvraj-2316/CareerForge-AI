46b68e47199014f6dec3e2e7f5cd7c6b813f8704
Yuvraj <yuvraj@Yuvrajs-MacBook-Air.local>
2026-09-25 03:03:02 +0530

    chore: initialize React, Express and MongoDB

---
+++ server/src/server.js
@@ -0,0 +1,30 @@
+
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "CareerForge AI backend is running!"
+  });
+});
+
+const PORT = process.env.PORT || 5001;
+
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
